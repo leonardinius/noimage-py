b462d3532a040a8940a28e9f4f275c33312d3650
Leonids Maslovs <leonids.maslovs@galeoconsulting.com>
2012-08-28 19:54:37 +0300

    POC version

--- noimage.py
+++ noimage.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 '''
 Created on Aug 27, 2012
 
@@ -5,6 +6,11 @@ Created on Aug 27, 2012
 '''
 
 from PIL import Image, ImageDraw, ImageFont
+from flask import Flask
+import flask
+import io
+import tempfile
+import re
 
 def draw(size, bgcolor=None, txtcolor=None, text=None):
     width, height = size
@@ -24,10 +30,85 @@ def draw(size, bgcolor=None, txtcolor=None, text=None):
         i += 2
     return mask
 
-if __name__ == '__main__':
-    c = 500
-    with open('output.png', 'wb') as f:
-        draw((c, c), (171, 140, 197), 'white').save(f)
+app = Flask(__name__)
+
+@app.route("/<path:path>")
+def serve_image(path):
+    # special pre-defined sizes to use in lieu of number dimensions.  taken from dummyimage.com.
+    sizes = {
+        # Ad Sizes
+        'mediumrectangle':      '300x250',
+        'squarepopup':          '250x250',
+        'verticalrectangle':    '240x400',
+        'largerectangle':        '336x280',
+        'rectangle':            '180x150',
+        'popunder':             '720x300',
+        'fullbanner':           '468x60',
+        'halfbanner':           '234x60',
+        'microbar':             '88x31',
+        'button1':              '120x90',
+        'button2':              '120x60',
+        'verticalbanner':        '120x240',
+        'squarebutton':         '125x125',
+        'leaderboard':          '728x90',
+        'wideskyscraper':        '60x600',
+        'skyscraper':           '120x600',
+        'halfpage':             '300x600',
         
-    with open('output2.png', 'wb') as f:
-        draw((c, c), (171, 140, 197), 'white', 'asas\nrtrt').save(f)
+        # Screen Resolutions
+        'cga':      '320x200',
+        'qvga':        '320x240',
+        'vga':        '640x480',
+        'wvga':        '800x480',
+        'svga':        '800x480',
+        'wsvga':    '1024x600',
+        'xga':       '1024x768',
+        'wxga':        '1280x800',
+        'wsxga':     '1440x900',
+        'wuxga':     '1920x1200',
+        'wqxga':    '2560x1600',
+        
+        # Video Resolutions
+        'ntsc':     '720x480',
+        'pal':      '768x576',
+        'hd720':    '1280x720',
+        'hd1080':    '1920x1080',
+    }
+    
+    mimetypes = {
+                 'tiff' : 'image/tiff' 
+    }
+    
+    spec = re.match(r"(?P<actual_size>[a-zA-Z0-9x]+)(\.(?P<ext>gif|jpeg|png|bmp))?$", path)
+    if spec:
+        size_name = spec.group('actual_size')
+        actual_size = sizes.get(size_name, size_name)
+        image_size = tuple(int(x) for x in actual_size.split('x'))
+        ext = spec.group('ext') or 'png'
+        img = draw(image_size, (171, 140, 197), 'white', actual_size)
+        def get_bytes(file):
+            with file as f:
+                img.save(f, ext)
+                f.flush()
+                f.seek(0)
+                return flask.Response(f.read(), mimetype=mimetypes.get(ext, 'image/' + ext))
+        try:
+            return get_bytes(io.BytesIO())
+        except io.UnsupportedOperation, e:
+            """some of the formats do not support save operation into memory stream
+            and are expecting file objects with fileno method support
+            """
+            if e.args == ('fileno',):
+                return get_bytes(tempfile.TemporaryFile())
+            raise
+            
+    else:
+        return flask.render_template('page404.html'), 404
+
+@app.route("/")
+@app.route("/index.html")
+def index():
+    return flask.render_template("index.html")    
+
+if __name__ == '__main__':
+    app.run(debug=True)
