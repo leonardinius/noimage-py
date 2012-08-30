ebc9931226a8accbd2405795579d29cef00c6e4f
Leonids Maslovs <leonids.maslovs@galeoconsulting.com>
2012-08-30 22:57:46 +0300

    Deployement improvements

--- noimageyet.py
+++ noimageyet.py
@@ -9,19 +9,33 @@ from PIL import Image, ImageDraw, ImageFont
 from flask.ext.bootstrap import Bootstrap
 import flask
 import io
+import json
 import os
 import re
+
+def loadConfig(path=os.getcwd()):
+    file_name = os.path.join(path, '.config.json')
+    if not os.path.isfile(file_name):
+        return None
+    return json.load(open(file_name))
+
+
+cfg = loadConfig()
+app = flask.Flask(__name__)
+Bootstrap(app)
+app.config['BOOTSTRAP_USE_CDN'] = cfg.get('BOOTSTRAP_USE_CDN', True)
+app.config['BOOTSTRAP_USE_MINIFIED'] = cfg.get('BOOTSTRAP_USE_MINIFIED', True)
 
 def draw(size, bgcolor=None, txtcolor=None, text=None):
     width, height = size
     
     if not text:
-        text = "%sx%s" % size
+        text = '%sx%s' % size
     lines = text.splitlines()
     
     mask = Image.new('RGB', size, bgcolor)
     draw = ImageDraw.Draw(mask)
-    font = ImageFont.truetype(os.getenv("NOIMAGEYET_TTF", "Arial.ttf"), min(int(height / max(5, len(lines) + 1)), int(1.5 * width / (max(len(l) for l in lines)))))
+    font = ImageFont.truetype(cfg.get('NOIMAGEYET_TTF', 'Arial.ttf'), min(int(height / max(5, len(lines) + 1)), int(1.5 * width / (max(len(l) for l in lines)))))
     
     i = -0.5 if len(lines) == 1 else int(-1 * len(lines) / 2) 
     for text_line in lines:
@@ -30,12 +44,8 @@ def draw(size, bgcolor=None, txtcolor=None, text=None):
         i += 1
     return mask
 
-app = flask.Flask(__name__)
-Bootstrap(app)
-app.config['BOOTSTRAP_USE_CDN'] = False
-app.config['BOOTSTRAP_USE_MINIFIED'] = True
 
-@app.route("/<path:path>")
+@app.route('/<path:path>')
 def serve_image(path):
     # special pre-defined sizes to use in lieu of number dimensions.  taken from dummyimage.com.
     #
@@ -84,12 +94,12 @@ def serve_image(path):
                  'tiff' : 'image/tiff' 
     }
     
-    spec = re.match(r"(?P<actual_size>[a-zA-Z0-9x]+)?(/(?P<bgcolor>[a-zA-Z0-9\#\,\%\\)\\(]+))?(/(?P<txtcolor>[a-zA-Z0-9\#\,\%\\)\\(]+))?(\.(?P<ext>gif|jpeg|png|bmp))?$", path)
+    spec = re.match(r'(?P<actual_size>[a-zA-Z0-9x]+)?(/(?P<bgcolor>[a-zA-Z0-9\#\,\%\\)\\(]+))?(/(?P<txtcolor>[a-zA-Z0-9\#\,\%\\)\\(]+))?(\.(?P<ext>gif|jpeg|png|bmp))?$', path)
     
     class MyBytesIOHack(io.BytesIO):
-        """
+        '''
         https://bugs.launchpad.net/ubuntu/+source/python-imaging/+bug/718852
-        """
+        '''
         def __init__(self, *args, **kwargs):
             io.BytesIO.__init__(self, *args, **kwargs)
         
@@ -120,7 +130,7 @@ def serve_image(path):
 @app.route('/', defaults={'page': 'index'})
 @app.route('/<string:page>.html')
 def view(page):
-    return flask.render_template("%s.html" % page,
+    return flask.render_template('%s.html' % page,
                                  page={'active_page' : page})    
 
 if __name__ == '__main__':
